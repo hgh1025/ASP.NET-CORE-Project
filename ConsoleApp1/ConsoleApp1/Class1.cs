72c166d91b70f3f85b2fcf0eba9ab311d2c0ce6f
지훈 허 <https://github.com/hgh1025>
2023-09-07 11:39:14 +0900

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public int Id { get; set; }
+
+        public string name { get; set; }
+
+        public static readonly string DefaultCookiePrefix;
+   
+        public void FullName()
+        {
+            
+        }
+    }
+}
